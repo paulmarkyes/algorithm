66d4fe30959333f0df3cce52ff767ed4f921804f
paulmarkyes <fxygt4995291@126.com>
2017-11-12 21:51:28 +0800

    队列节点类

---
+++ data_structure/src/algorithms/Queue_Node.java
@@ -0,0 +1,12 @@
+package algorithms;
+
+public class Queue_Node<Key> {
+	Key key;
+	Queue_Node<Key> next;
+	public Queue_Node(Key key) {
+		// TODO Auto-generated constructor stub
+		this.key=key;
+	}
+	
+
+}
